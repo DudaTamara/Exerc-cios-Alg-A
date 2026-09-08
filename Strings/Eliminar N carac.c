76df4a617a894517890ed345b995be23c804c5f3
DudaTamara <dudatamara2026@gmail.com>
2026-09-08 20:25:22 -0300

    arrumado

--- Strings/Eliminar N carac.c
+++ Strings/Eliminar N carac.c
@@ -1,7 +1,7 @@
 /*Escrever um programa para ler uma string e um número n, e eliminar os n caracteres do final da string. 
 A string resultante deve ser mostrada na tela. Por exemplo, lida a string “Agora” e o número 3, deve mostrar na tela a palavra “Ag”.*/
 
-#include<studio.h>
+#include<stdio.h>
 
 int main (){
 char str[30]; 
@@ -13,12 +13,8 @@ gets(str);
 printf("Digite o numeo de letras a serem cortadas no final: "); 
 scanf("%d",&n); 
 
-for(i=0; str[i];i++); 
-if(i<=n) 
-str[0]='\0'; 
-
-else
-str[i-n]='\0'; 
+for(i = 0; str[i]; i++) { }
+str[i - n] = '\0';
 
 printf("string resultante: %s",str); 
 }
